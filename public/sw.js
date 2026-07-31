cbdd43801f0d3b8faf54d1c647c537827a69a0fd
anu-aditya <anuragaditya06@gmail.com>
2026-07-31 20:34:07 +0530

    adsterra to monetag

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11063469
+    "zoneId": 11461732
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
